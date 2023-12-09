911e849403d0c0d0c45b0ab5d110c7fe9f33289a
Kronos <dimamitru@gmail.com>
2023-12-09 13:32:47 +0200

    Add Menu Items Details Page

--- book_worm_client/src/Container/App.tsx
+++ book_worm_client/src/Container/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Footer, Header } from "../Components/Layout";
-import { Home, NotFound } from "../Pages";
+import { Home, MenuItemsDetails, NotFound } from "../Pages";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,6 +10,10 @@ function App() {
       <div className="pb-5">
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/menuItemDetails/:menuitemId"
+            element={<MenuItemsDetails />}
+          ></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </div>
--- book_worm_client/src/Pages/index.ts
+++ book_worm_client/src/Pages/index.ts
@@ -1,4 +1,5 @@
 import Home from "./Home";
 import NotFound from "./NotFound";
+import MenuItemsDetails from "./MenuItemsDetails";
 
-export {Home,NotFound};
+export {Home,NotFound,MenuItemsDetails};
