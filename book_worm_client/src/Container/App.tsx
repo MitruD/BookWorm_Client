8dbbe9a15c797e238b81077f29af94c4eaec2f03
Kronos <dimamitru@gmail.com>
2023-12-13 22:17:55 +0200

    Get Shopping Cart and Store in Slice

--- book_worm_client/src/Container/App.tsx
+++ book_worm_client/src/Container/App.tsx
@@ -2,8 +2,25 @@ import React from "react";
 import { Footer, Header } from "../Components/Layout";
 import { Home, MenuItemDetails, NotFound } from "../Pages";
 import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useGetShoppingCartQuery } from "../Apis/shoppingCartApi";
+import { setShoppingCart } from "../Storage/Redux/shoppingCartSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const { data, isLoading } = useGetShoppingCartQuery(
+    "a54b4f2d-8282-4da7-a9a7-e4149e0611a0"
+  );
+
+  useEffect(() => {
+    if (!isLoading) {
+      console.log(data.result);
+      dispatch(setShoppingCart(data.result?.cartItems));
+    }
+  }, [data]);
+
   return (
     <div>
       <Header />
