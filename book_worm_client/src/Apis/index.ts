2161997b099c6bea4c48f888b61c78cbc14c5f01
Kronos <dimamitru@gmail.com>
2023-12-25 16:34:52 +0200

    Create Payment API

--- book_worm_client/src/Apis/index.ts
+++ book_worm_client/src/Apis/index.ts
@@ -1,5 +1,6 @@
 import authApi from "./authApi";
 import menuItemApi from "./menuItemApi";
 import shoppingCartApi from "./authApi";
+import paymentApi from "./paymentApi";
 
-export {menuItemApi,shoppingCartApi,authApi};
+export { menuItemApi, shoppingCartApi, authApi, paymentApi };
