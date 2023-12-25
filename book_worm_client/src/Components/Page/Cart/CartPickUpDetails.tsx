a70accf3563f439de244882a35e5b666d297515f
Kronos <dimamitru@gmail.com>
2023-12-25 15:56:03 +0200

    Payments: Load User Details of Logged in User

--- book_worm_client/src/Components/Page/Cart/CartPickUpDetails.tsx
+++ book_worm_client/src/Components/Page/Cart/CartPickUpDetails.tsx
@@ -10,13 +10,14 @@ function CartPickUpDetails() {
   const shoppingCartFromStore: cartItemModel[] = useSelector(
     (state: RootState) => state.shoppingCartStore.cartItems ?? []
   );
+  const userData = useSelector((state: RootState) => state.userAuthStore);
 
   let grandTotal = 0;
   let totalItems = 0;
 
   const initialUserData = {
-    name: "",
-    email: "",
+    name: userData.fullName,
+    email: userData.email,
     phoneNumber: "",
   };
 
