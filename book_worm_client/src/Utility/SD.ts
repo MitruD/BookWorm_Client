4135730dad74d4e1b979c8de3f4008c906f8b8ab
Kronos <dimamitru@gmail.com>
2023-12-28 14:26:12 +0200

    Create Order Object to Create Order

--- book_worm_client/src/Utility/SD.ts
+++ book_worm_client/src/Utility/SD.ts
@@ -1,5 +1,14 @@
 //SD - Static detail
 export enum SD_Roles {
-    ADMIN = "admin",
-    CUTOMER = "customer",
-  }
+  ADMIN = "admin",
+  CUTOMER = "customer",
+}
+
+export enum SD_Status {
+  PENDING = "Pending",
+  CONFIRMED = "Confirmed",
+  BEING_COOKED = "Being Cooked",
+  READY_FOR_PICKUP = "Ready for Pickup",
+  COMPLETED = "Completed",
+  CANCELLED = "Cancelled",
+}
